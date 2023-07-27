c5e244f8cd614287a5bbcabd9c19484989307716
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-07-27 17:33:24 +0530

    Added signup and signin features along with few UI modernization

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Microblog
+## Microblogging Project
+
+### Micro-Blogger
+
+Welcome to the Microblogging project! This is a simple microblogging platform built using Bootstrap HTML, CSS, Flask, and MongoDB. The project allows users to create an account, post short messages and explore the latest posts from other people.
+
+### Features
+
+#### SignUp Module
+* User Registration: Users can sign up for an account using a valid email address and password.
+* Validation: Input fields are validated to ensure proper email format and password strength.
+
+#### SignIn Module
+* User Login: Registered users can log in to the platform with their credentials.
+* Authentication: Secure authentication mechanisms are in place to protect user accounts.
+
+#### Home Module
+* Timeline: Users can view the latest posts from all the users on their timeline.
+* Create Posts: Users can create and share short posts with others.
+
+#### Profile Module
+* User Profile: Each user has a profile page displaying their posts.
+
+### Technologies Used
+* Bootstrap HTML/CSS: For responsive and visually appealing frontend design.
+* Flask: A lightweight Python web framework for handling backend logic and routing.
+* Jinja2: A powerful and flexible templating engine for rendering dynamic content in HTML templates.
+* MongoDB: A NoSQL database used to store user information and posts.
+
+### Prerequisites
+
+Before running the application, ensure you have the following installed:
+
+Python (version 3.6 or higher)
+MongoDB
+Flask
+
+### License
+
+This project is licensed under the MIT License.
+
+### Acknowledgments
+
+* The Bootstrap team for their fantastic frontend framework.
+* The Flask team for their lightweight and efficient web framework.
+* The MongoDB team for their robust NoSQL database.
+* The Jinja2 team for their powerful templating engine.
+
+### Contact
+
+If you have any questions or suggestions, feel free to contact us at abhijitpaul@gmail.com.
+
+Happy Microblogging!
--- app.py
+++ app.py
@@ -1,26 +1,102 @@
 import datetime
 import os
-from flask import Flask, render_template, request
+import functools
+
+from flask import (
+    Flask,
+    session,
+    render_template,
+    request,
+    abort,
+    flash,
+    redirect,
+    url_for
+)
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from passlib.hash import pbkdf2_sha256
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.getenv("SECRET_KEY")
+
     client = MongoClient(os.getenv("MONGODB_URI"))
     app.db = client.microblog
 
-    @app.route("/", methods=["GET", "POST"])
+    def login_required(route):
+        @functools.wraps(route)
+        def route_wrapper(*args, **kwargs):
+            email = session.get("email")
+            print("Session Email:", email)
+            users = {user.get("email"): user.get("password") for user in app.db.users.find({})}
+            if email not in users:
+                return redirect(url_for("login"))
+            return route(*args, **kwargs)
+        return route_wrapper
+
+    @app.route("/home", methods=["GET", "POST"])
+    @login_required
     def home():
+        entries_with_date = [(entry.get("email"), entry.get("content"), entry.get("date"), datetime.datetime.strptime(entry.get("date"), "%Y-%m-%d").strftime("%b %d")) for entry in app.db.entries.find({})]
+        entries_with_date.reverse()
+        return render_template("home.html", entries=entries_with_date)
+
+    @app.route('/', methods=["GET", "POST"])
+    def login():
+        email = ""
+        if request.method == "POST":
+            email = request.form.get("email")
+            password = request.form.get("password")
+            
+            users = {user.get("email"): user.get("password") for user in app.db.users.find({})}
+            print(users)
+            
+            # if email in users and users.get(email) == password:
+            if email in users:
+                if pbkdf2_sha256.verify(password, users.get(email)):
+                    session["email"] = email
+                    return redirect(url_for('profile'))
+            flash("Incorrect e-mail or password.")
+        return render_template('login.html', email=email)
+
+    @app.route('/signup', methods=["GET", "POST"])
+    def signup():
+        if request.method == "POST":
+            email = request.form.get("email")
+            password = request.form.get("password")
+            # Before saving, we need to check for uniqueness of user id
+            users = {user.get("email"): user.get("password") for user in app.db.users.find({})}
+            if email not in users:
+                # saving users data
+                app.db.users.insert_one({"email": email, "password":  pbkdf2_sha256.hash(password)})
+                flash("Successfully signed up.")
+                return redirect(url_for('login'))
+            else:
+                flash("Account already exists with this email address. Try another one!")
+        return render_template("signup.html")
+
+    @app.route("/profile", methods=["GET", "POST"])
+    @login_required
+    def profile():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+            app.db.entries.insert_one({"email": session.get("email"), "content": entry_content, "date": formatted_date})
+            return redirect(url_for('profile'))
         
-        entries_with_date = [(entry.get("content"), entry.get("date"), datetime.datetime.strptime(entry.get("date"), "%Y-%m-%d").strftime("%b %d")) for entry in app.db.entries.find({})]
+        entries_with_date = [(entry.get("email"), entry.get("content"), entry.get("date"), datetime.datetime.strptime(entry.get("date"), "%Y-%m-%d").strftime("%b %d")) for entry in app.db.entries.find({'email': session.get("email")})]
+        entries_with_date.reverse()
+        return render_template("profile.html", entries=entries_with_date)
 
-        return render_template("home.html", entries=entries_with_date)
+    @app.route("/signout")
+    def signout():
+        if 'email' in session:  
+            session.pop('email',None)  
+            return redirect(url_for('login'))  
+        else:  
+            return '<p>user already logged out</p>'  
     return app
 
 # if __name__ == "__main__":
